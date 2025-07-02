93deba88260003454ea1c021339c37e0fb2b88b8
RiyaKachhara <riyakachhara1144@gmail.com>
2025-07-02 21:11:50 +0530

    changes

--- App.jsx
+++ App.jsx
@@ -1,37 +1,3 @@
-
-// import React from 'react';
-// import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import StocksStackNavigator from './src/navigation/StocksStackNavigator';
-// import WatchlistStackNavigator from './src/navigation/WatchlistStackNavigator';
-// import Toast from 'react-native-toast-message';
-// import Icon from 'react-native-vector-icons/Feather'; 
-
-// import { ThemeProvider, useTheme } from './src/utils/ThemeContext';
-
-// const Tab = createBottomTabNavigator();
-
-// function AppContent() {
-//   const { isDark } = useTheme();
-//   return (
-//     <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
-//       <Tab.Navigator screenOptions={{ headerShown: false }}>
-//         <Tab.Screen name="Home" component={StocksStackNavigator} />
-//         <Tab.Screen name="Watchlist" component={WatchlistStackNavigator} />
-//       </Tab.Navigator>
-//       <Toast />
-//     </NavigationContainer>
-//   );
-// }
-
-// export default function App() {
-//   return (
-//     <ThemeProvider>
-//       <AppContent />
-//     </ThemeProvider>
-//   );
-// }
-
 import React from 'react';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
